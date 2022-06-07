bbc9cf9deeb7ac0153c6a326bd070623859e8ffa
Gideon Nimoh <gnimoh001@gmail.com>
2022-06-07 23:48:16 +0200

    fixed flake8 linting issues

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,6 +1,4 @@
-"""
-    Test custom Django management commands
-"""
+""" Test custom Django management commands """
 
 from unittest.mock import patch
 
@@ -13,31 +11,18 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
-    """
-        Test commands
-    """
+    """ Test commands """
     def test_wait_for_db_success(self, patched_check):
-        """
-            Test waiting for db when db is available
-        """
+        """ Test waiting for db when db is available """
         patched_check.return_value = True
-        
         call_command('wait_for_db')
-        
         patched_check.assert_called_once_with(databases=["default"])
-    
-    
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """
-        Test waiting for database when getting OperationalError
-        """
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
-        
+        """ Test waiting for database when getting OperationalError """
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
         call_command('wait_for_db')
-        
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=["default"])
-    
-    
-        
