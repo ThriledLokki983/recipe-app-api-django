6f074fa679045d415b0d4e1c38431a2a9aed147f
Gideon Nimoh <gnimoh001@gmail.com>
2022-06-08 10:34:33 +0200

    Customizing Django Admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,42 @@
-from django.contrib import admin # noqa
+"""
+Django Admin customization
+"""
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from typing import Sequence
+
+from core import models
 # Register your models here.
+
+
+# @admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'last_login',
+                )
+            }
+        )
+    )
+    readonly_fields = ['last_login']
+
+admin.site.register(models.User, UserAdmin)
+
